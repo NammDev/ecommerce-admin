7fa166627c213805f7ca87f6b219bad8223f5a6c
Nam <namdeveloper.ca@gmail.com>
2023-10-17 10:43:50 +0700

    setup clerK

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div className='p-4'>
       <Button variant={'default'}>Click me</Button>
+      <UserButton afterSignOutUrl='/' />
     </div>
   )
 }
