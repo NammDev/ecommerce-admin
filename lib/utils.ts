16365287374f6afd56094a8ca732f88f292c3c3a
Nam <namdeveloper.ca@gmail.com>
2023-10-22 23:20:36 +0700

    just for streak

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// calculate the class names
+
+const data = [
+  [
+    { name: 'John', age: 25 },
+    { name: 'Jane', age: 30 },
+  ],
+  [{ name: 'Bob', age: 40 }],
+]
+
+// map through an array of objects
+const names = data.map((item) => item.map((i) => i.name))
