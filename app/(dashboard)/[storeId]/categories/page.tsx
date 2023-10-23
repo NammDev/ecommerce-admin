3547f7f2ceb1e952c6b5bb468824fa2f422d6923
Nam <namdeveloper.ca@gmail.com>
2023-10-23 18:26:44 +0700

    client category

--- app/(dashboard)/[storeId]/categories/page.tsx
+++ app/(dashboard)/[storeId]/categories/page.tsx
@@ -1,12 +1,35 @@
+import { db } from '@/db'
+import { format } from 'date-fns'
 import React from 'react'
+import { CategoryColumn } from './component/columns'
+import { CategoriesClient } from './component/client'
 
 interface CategoriesProps {
-  // Define any props you need here
+  params: { storeId: string }
 }
 
-const Categories: React.FC<CategoriesProps> = (props) => {
+const Categories: React.FC<CategoriesProps> = async ({ params }) => {
   // Implement your component logic here
-  return <div>{/* Render your component UI here */}</div>
+  const categories = await db.category.findMany({
+    where: { storeId: params.storeId },
+    include: { billboard: true },
+    orderBy: { createdAt: 'desc' },
+  })
+
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }))
+
+  return (
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <CategoriesClient data={formattedCategories} />
+      </div>
+    </div>
+  )
 }
 
 export default Categories
