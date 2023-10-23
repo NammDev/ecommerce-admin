dc09f0a52d6b6de2d42fd28cf64d5245180e5b9b
Nam <namdeveloper.ca@gmail.com>
2023-10-23 15:57:09 +0700

    List all billboards

--- app/(dashboard)/[storeId]/billboards/page.tsx
+++ app/(dashboard)/[storeId]/billboards/page.tsx
@@ -1,16 +1,33 @@
-import BillboardClient from '@/components/share/BillboardClient'
+import { BillboardClient } from '@/components/share/BillboardClient'
+import { BillboardColumn } from '@/components/share/BillboardColumn'
+import { db } from '@/db'
+import { format } from 'date-fns'
 import React from 'react'
 
 interface Props {
-  storeId: string
+  params: { storeId: string }
 }
 
-const BillboardsPage: React.FC<Props> = ({ storeId }) => {
-  // Your component logic here
+const BillboardsPage: React.FC<Props> = async ({ params }) => {
+  const billboards = await db.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }))
+
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
-        <BillboardClient />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   )
--- components/share/BillboardClient.tsx
+++ components/share/BillboardClient.tsx
@@ -1,28 +1,37 @@
 'use client'
-import React from 'react'
-import { Heading } from './Heading'
-import { Button } from '../ui/button'
+
 import { Plus } from 'lucide-react'
-import { Separator } from '../ui/separator'
 import { useParams, useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { BillboardColumn, columns } from './BillboardColumn'
+import { DataTable } from '@/components/ui/data-table'
+import { Heading } from './Heading'
 
-interface Props {}
+interface BillboardClientProps {
+  data: BillboardColumn[]
+}
 
-const BillboardClient: React.FC<Props> = () => {
-  const router = useRouter()
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const params = useParams()
+  const router = useRouter()
+
   return (
     <>
       <div className='flex items-center justify-between'>
-        <Heading title='Billboards (0)' description='Manage Billboard for your store' />
+        <Heading
+          title={`Billboards (${data.length})`}
+          description='Manage billboards for your store'
+        />
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-          <Plus className='mr-2 h-4 w-4' />
-          Add New
+          <Plus className='mr-2 h-4 w-4' /> Add New
         </Button>
       </div>
       <Separator />
+      <DataTable searchKey='label' columns={columns} data={data} />
+      <Heading title='API' description='API Calls for Billboards' />
+      <Separator />
+      {/* <ApiList entityName='billboards' entityIdName='billboardId' /> */}
     </>
   )
 }
-
-export default BillboardClient
