27a35bfc4b8126a4f27cd107f7df64198cdb80d1
Nam <namdeveloper.ca@gmail.com>
2023-11-04 19:47:12 +0700

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as ToasterHot } from 'react-hot-toast'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,10 +19,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
-          <ModalProvider />
-          {children}
-          <Toaster />
-          <ToasterHot />
+          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+            <ModalProvider />
+            {children}
+            <Toaster />
+            <ToasterHot />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/share/Navbar.tsx
+++ components/share/Navbar.tsx
@@ -3,6 +3,7 @@ import { UserButton, auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 import StoreSwitcher from './StoreSwitcher'
 import { MainNav } from './MainNav'
+import { ThemeToggle } from './ThemeToggle'
 
 const Navbar = async () => {
   const { userId } = auth()
@@ -12,11 +13,11 @@ const Navbar = async () => {
 
   return (
     <div className='border-b'>
-      <div className='flex h-16 items-center px-4'>
+      <div className='flex items-center h-16 px-4'>
         <StoreSwitcher items={stores} />
         <MainNav className='mx-6' />
-        <div className='ml-auto flex items-center space-x-4'>
-          {/* <ThemeToggle /> */}
+        <div className='flex items-center ml-auto space-x-4'>
+          <ThemeToggle />
           <UserButton afterSignOutUrl='/' />
         </div>
       </div>
